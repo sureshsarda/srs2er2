92eefc86fb945913abdf2e2a2a693db895c300fd
SureshSarda <SureshSarda@Suresh>
2015-02-03 10:15:51 +0530

    classes added: Tag, TagDataLoader enum added: TagType
    costmatrix of tags

--- srs2er(v3)/src/nlp/objects/Tag.java
+++ srs2er(v3)/src/nlp/objects/Tag.java
@@ -1,46 +1,29 @@
 package nlp.objects;
 
-public class Tag implements Comparable<Tag>{
+import nlp.objects.TagDataLoader.TagType;
 
-	protected String tag;
-	
-	public enum TAG_DIFFERENCE {
-		SAME, FAMILY, DIFFERENT
-	}
-	
-	public static Integer costDifference(Tag tag1, Tag tag2) {
-		return 0;
-	}
-	
-	public static TAG_DIFFERENCE costDifference(String tag1, String tag2) {
-		if (tag1.charAt(0) == tag2.charAt(0)) {
-			if (tag1.charAt(1) == tag2.charAt(1)) {
-				return TAG_DIFFERENCE.SAME;
-			}
-			else {
-				return TAG_DIFFERENCE.FAMILY;
-			}
-		}
-		else {
-			return TAG_DIFFERENCE.DIFFERENT;
-		}
-	}
-	
-	
-	public boolean equals(Tag tag) {
-		return false;
-	}
-	
-	@Override
-	public String toString() {
-		return null;
-	}
+public class Tag  {
 
-	@Override
-	public int compareTo(Tag tag) {
+    public enum TAG_DIFFERENCE {
+	SAME, FAMILY, DIFFERENT
+    }
 
-		return 0;
+    TagType tagType;
+    
+    public Tag(String name) {
+	tagType = TagType.valueOf(name);
+    }
+
+    public static TAG_DIFFERENCE costDifference(String tag1, String tag2) {
+	if (tag1.charAt(0) == tag2.charAt(0)) {
+	    if (tag1.charAt(1) == tag2.charAt(1)) {
+		return TAG_DIFFERENCE.SAME;
+	    } else {
+		return TAG_DIFFERENCE.FAMILY;
+	    }
+	} else {
+	    return TAG_DIFFERENCE.DIFFERENT;
 	}
+    }
 
-	
 }
---
+++ srs2er(v3)/src/nlp/objects/TagDataLoader.java
@@ -0,0 +1,190 @@
+package nlp.objects;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * 
+ * @author SureshSarda
+ *
+ */
+
+
+/*
+ * 
+ * DO NOT AUTO FORMAT THIS FILE!
+ * (Do NOT save it if you do.
+ */
+
+/*Singleton Object*/
+public class TagDataLoader {
+
+    enum TagType {
+	UNKNOWN(00),
+	NN(1, 	"Noun (Singular)"),
+	NNS(2,	"Noun (Plural)"), 
+	NNP(3, 	"ProperNoun (Singular)"), 
+	NNPS(4, "ProperNoun (Plural)"), 
+	
+	VB(5, 	"Verb (Base Form)"), 
+	VBD(6, 	"Verb (Past Tense)"), 
+	VBG(7, 	"Verb (Gerund/Present Participle)"), 
+	VBN(8, 	"Verb (Past Participle"), 
+	VBP(9, 	"Verb (Singular Present, Non 3rd person)"), 
+	VBZ(10, "Verb (Singular Present, 3rd Person)"), 
+	
+	RB(11, 	"Adverb"), 
+	RBR(12, "Adverb (Comparative)"),
+	RBS(13, "Adverb (Superlative)"), 
+	
+	JJ(14, 	"Adjective"), 
+	JJR(15, "Adjective (Comparative)"),
+	JJS(16, "Adjective (Superlative)"),
+	
+	PRP(17, "Personal Pronoun"), 
+	PRP$(18,"Possessive Pronoun"), 
+	WP(19, 	"Wh­pronoun"), 
+	WP$(20, "Possessive Wh­Pronoun"), 
+	CC(21, 	"Coordinating Conjunction"), 
+	CD(22, 	"Cardinal Number"), 
+	DT(23), 
+	EX(24), 
+	FW(25), 
+	IN(26), 
+	LS(27), 
+	MD(28), 
+	PDT(29), 
+	POS(30), 
+	RP(31), 
+	SYM(32), 
+	TO(33), 
+	UH(34), 
+	WDT(35), 
+	WRB(36);
+
+
+	final static int count = 36;
+
+	int value;
+	String longString;
+	int deletionCost;
+	int insertionCost;
+	List<Integer> substitutionCost;
+
+	TagType(int val) {
+	    value = val;
+	}
+	
+	TagType(int val, String longString) {
+	    value = val;
+	    setLongString(longString);
+	}
+
+	void setLongString(String longString) {
+	    this.longString = longString;
+	}
+	
+	void setInsertionCost(int ins) {
+	    insertionCost = ins;
+	}
+	void setDeletionCost(int del) {
+	    deletionCost = del;
+	}
+	void setSubstutionCost(List<Integer> subs) {
+	    substitutionCost = subs;
+	}
+	
+	int getInsertionCost() {
+	    return insertionCost;
+	}
+	
+	int getDeletionCost() {
+	    return deletionCost;
+	}
+	
+	int getSubstitutionCost(TagType subsWith) {
+	    return substitutionCost.get(subsWith.ordinal());
+	}
+	
+	int getValue() {
+	    return value;
+	}
+
+	public String toString() {
+	    return this.name();
+	}
+
+    };
+    /* TagType Enum ends here */
+    
+    
+    
+    private TagDataLoader instance;
+    
+    public TagDataLoader getInstance() throws IOException {
+	if (instance == null) {
+	    instance = new TagDataLoader();
+	}
+	return instance;
+    }
+    
+    private TagDataLoader() throws IOException {
+	initTagCostMatrices();
+    }
+
+
+    File costMatrixFile = new File("data\\post\\cost.csv");
+
+    private void initTagCostMatrices() throws IOException {
+	Iterator<String> lineIterator = Files.lines(costMatrixFile.toPath()).iterator();
+
+	String firstLine = lineIterator.next();
+	String insertion = lineIterator.next();
+	String deletion = lineIterator.next();
+	
+	checkTagSequence(firstLine);
+	assignCosts(insertion.split(","), deletion.split(","));
+	
+	while (lineIterator.hasNext()) {
+	    assignCosts(lineIterator.next().split(","));
+	}
+
+    }
+    private void assignCosts(String[] insertion, String[] deletion) {
+	for (int i = 1; i < TagType.count; i++) {
+	    TagType current = TagType.values()[i];
+	    
+	    current.setInsertionCost(Integer.parseInt(insertion[i]));
+	    current.setDeletionCost(Integer.parseInt(deletion[i]));
+	}
+    }
+    
+    private void assignCosts(String[] subs) {
+	List<Integer> subsCost = new ArrayList<Integer>(subs.length);
+	for (int i = 1; i < TagType.count; i++) {
+	    subsCost.add(Integer.parseInt(subs[i]));
+	}
+	TagType.valueOf(subs[0]).setSubstutionCost(subsCost);
+    }
+    
+    private boolean checkTagSequence(String firstLine) {
+	return true;
+    }
+
+    // FIXME UNWANTED MAIN
+    /*
+    public static void main(String[] args) throws IOException {
+	TagDataLoader tags = new TagDataLoader();
+	for (TagType tag : TagType.values()) {
+	    System.out.println(tag.longString!=null?tag.longString:tag.toString());
+	    System.out.println(String.format("Ins: %d, Del: %d", tag.insertionCost, tag.deletionCost));
+	    System.out.println(tag.substitutionCost);
+	}
+    }
+    */
+}
