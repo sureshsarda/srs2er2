965dc1b19b06868dc083f8fdd43d139db5ba76d5
SureshSarda <SureshSarda@Suresh>
2015-03-31 08:24:52 +0530

    Implement Collection Interface

--- srs2er(v3)/src/nlp/objects/Sentences.java
+++ srs2er(v3)/src/nlp/objects/Sentences.java
@@ -1,6 +1,8 @@
 package nlp.objects;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -11,33 +13,113 @@ import javax.xml.bind.annotation.XmlRootElement;
  *
  */
 
-@XmlRootElement(name="Sentences")
-public class Sentences {
+@XmlRootElement(name = "Sentences")
+public class Sentences implements Collection<Sentence>
+{
 
 	@XmlElement(name = "Sentence")
-	private ArrayList<Sentence> SentenceList;
+	private ArrayList<Sentence> sentenceList;
 
-	public ArrayList<Sentence> getSentence() {
-		return SentenceList;
+	public ArrayList<Sentence> getSentence()
+	{
+		return sentenceList;
 	}
-	public void setSentence(ArrayList<Sentence> SentenceList) {
-		this.SentenceList = SentenceList;
+	public void setSentence(ArrayList<Sentence> SentenceList)
+	{
+		this.sentenceList = SentenceList;
 	}
-	public void addSentence(ArrayList<Sentence> SentenceList) {
-		if (this.SentenceList == null) {
-			this.SentenceList = new ArrayList<Sentence>(100);
-		}
-		this.SentenceList.addAll(SentenceList);
-	}
-	
+
 	@Override
-	public String toString() {
+	public String toString()
+	{
 		StringBuilder sb = new StringBuilder();
-		for (Sentence sentence : SentenceList) {
+		for (Sentence sentence : sentenceList)
+		{
 			sb.append(sentence.toString());
 			sb.append(System.lineSeparator());
 		}
 		return sb.toString();
 	}
-	
+
+	/*
+	 * Collection Interface
+	 */
+	@Override
+	public boolean add(Sentence sentence)
+	{
+		if (this.sentenceList == null)
+		{
+			this.sentenceList = new ArrayList<Sentence>();
+		}
+		this.sentenceList.add(sentence);
+		return true;
+	}
+	@Override
+	public boolean addAll(Collection<? extends Sentence> SentenceList)
+	{
+		if (this.sentenceList == null)
+		{
+			this.sentenceList = new ArrayList<Sentence>(100);
+		}
+		this.sentenceList.addAll(SentenceList);
+		return true;
+	}
+	@Override
+	public void clear()
+	{
+		this.sentenceList.clear();
+
+	}
+	@Override
+	public boolean contains(Object arg0)
+	{
+		return this.sentenceList.contains(arg0);
+	}
+	@Override
+	public boolean containsAll(Collection<?> arg0)
+	{
+		return this.sentenceList.containsAll(arg0);
+
+	}
+	@Override
+	public boolean isEmpty()
+	{
+		return this.sentenceList.isEmpty();
+	}
+	@Override
+	public Iterator<Sentence> iterator()
+	{
+		return this.sentenceList.iterator();
+	}
+	@Override
+	public boolean remove(Object arg0)
+	{
+		return this.sentenceList.remove(arg0);
+	}
+	@Override
+	public boolean removeAll(Collection<?> arg0)
+	{
+		return this.sentenceList.retainAll(arg0);
+	}
+	@Override
+	public boolean retainAll(Collection<?> arg0)
+	{
+		return this.sentenceList.retainAll(arg0);
+	}
+	@Override
+	public int size()
+	{
+		return this.sentenceList.size();
+	}
+	@Override
+	public Object[] toArray()
+	{
+		return this.sentenceList.toArray();
+	}
+	@Override
+	public <T> T[] toArray(T[] arg0)
+	{
+		return this.sentenceList.toArray(arg0);
+	}
+
 }
